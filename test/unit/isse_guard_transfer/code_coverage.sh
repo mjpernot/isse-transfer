f228e0509eed728b8d43c0e14dfb5adcc0857669
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-05-01 13:36:37 +0100

    Update test/unit/isse_guard_transfer/code_coverage.sh v3.0.4

--- test/unit/isse_guard_transfer/code_coverage.sh
+++ test/unit/isse_guard_transfer/code_coverage.sh
@@ -10,6 +10,7 @@ echo ""
 echo "Running unit test modules in conjunction with coverage"
 coverage run -a --source=isse_guard_transfer test/unit/isse_guard_transfer/help_message.py
 coverage run -a --source=isse_guard_transfer test/unit/isse_guard_transfer/load_cfg.py
+coverage run -a --source=isse_guard_transfer test/unit/isse_guard_transfer/process.py
 coverage run -a --source=isse_guard_transfer test/unit/isse_guard_transfer/process_files.py
 coverage run -a --source=isse_guard_transfer test/unit/isse_guard_transfer/set_sftp_conn.py
 coverage run -a --source=isse_guard_transfer test/unit/isse_guard_transfer/transfer_file.py
