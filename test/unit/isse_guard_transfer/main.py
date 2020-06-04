fabce4019c02a2e3227e53ae5e7abf0a633af68f
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-06-04 08:20:12 +0100

    Update test/unit/isse_guard_transfer/main.py v3.0.4

--- test/unit/isse_guard_transfer/main.py
+++ test/unit/isse_guard_transfer/main.py
@@ -29,9 +29,37 @@ import mock
 # Local
 sys.path.append(os.getcwd())
 import isse_guard_transfer
+import lib.gen_libs as gen_libs
 import version
 
 __version__ = version.__version__
+
+
+class ProgramLock(object):
+
+    """Class:  ProgramLock
+
+    Description:  Class stub holder for gen_class.ProgramLock class.
+
+    Methods:
+        __init__ -> Class initialization.
+
+    """
+
+    def __init__(self, cmdline, flavor):
+
+        """Method:  __init__
+
+        Description:  Class initialization.
+
+        Arguments:
+            (input) cmdline -> Argv command line.
+            (input) flavor -> Lock flavor ID.
+
+        """
+
+        self.cmdline = cmdline
+        self.flavor = flavor
 
 
 class UnitTest(unittest.TestCase):
@@ -50,6 +78,10 @@ class UnitTest(unittest.TestCase):
         test_arg_valid_true -> Test arg_valid_val if returns true.
         test_arg_dir_true -> Test arg_dir_chk_crt if returns true.
         test_arg_dir_false -> Test arg_dir_chk_crt if returns false.
+        test_run_program -> Test run_program function.
+        test_programlock_true -> Test with ProgramLock returns True.
+        test_programlock_false -> Test with ProgramLock returns False.
+        test_programlock_id -> Test ProgramLock with flavor ID.
 
     """
 
@@ -64,6 +96,9 @@ class UnitTest(unittest.TestCase):
         """
 
         self.args_array = {"-c": "CfgFile", "-d": "CfgDir", "-A": "process",
+                           "-N": "SIPR"}
+        self.proglock = ProgramLock(["cmdline"], "FlavorID")
+        self.args_array2 = {"-c": "CfgFile", "-d": "CfgDir", "-A": "send",
                            "-N": "SIPR"}
 
     @mock.patch("isse_guard_transfer.gen_libs.help_func")
@@ -197,10 +232,12 @@ class UnitTest(unittest.TestCase):
 
         self.assertFalse(isse_guard_transfer.main())
 
-    @mock.patch("isse_guard_transfer.run_program")
+    @mock.patch("isse_guard_transfer.run_program",
+                mock.Mock(return_value=True))
+    @mock.patch("isse_guard_transfer.gen_class.ProgramLock")
     @mock.patch("isse_guard_transfer.gen_libs.help_func")
     @mock.patch("isse_guard_transfer.arg_parser")
-    def test_arg_dir_false(self, mock_arg, mock_help, mock_run):
+    def test_arg_dir_false(self, mock_arg, mock_help, mock_lock):
 
         """Function:  test_arg_dir_false
 
@@ -215,7 +252,105 @@ class UnitTest(unittest.TestCase):
         mock_arg.arg_require.return_value = False
         mock_arg.arg_valid_val.return_value = True
         mock_arg.arg_dir_chk_crt.return_value = False
-        mock_run.return_value = True
+        mock_lock.return_value = self.proglock
+
+        self.assertFalse(isse_guard_transfer.main())
+
+    @mock.patch("isse_guard_transfer.run_program",
+                mock.Mock(return_value=True))
+    @mock.patch("isse_guard_transfer.gen_class.ProgramLock")
+    @mock.patch("isse_guard_transfer.gen_libs.help_func")
+    @mock.patch("isse_guard_transfer.arg_parser")
+    def test_run_program(self, mock_arg, mock_help, mock_lock):
+
+        """Function:  test_run_program
+
+        Description:  Test run_program function.
+
+        Arguments:
+
+        """
+
+        mock_arg.arg_parse2.return_value = self.args_array
+        mock_help.return_value = False
+        mock_arg.arg_require.return_value = False
+        mock_arg.arg_valid_val.return_value = True
+        mock_arg.arg_dir_chk_crt.return_value = False
+        mock_lock.return_value = self.proglock
+
+        self.assertFalse(isse_guard_transfer.main())
+
+    @mock.patch("isse_guard_transfer.run_program",
+                mock.Mock(return_value=True))
+    @mock.patch("isse_guard_transfer.gen_class.ProgramLock")
+    @mock.patch("isse_guard_transfer.gen_libs.help_func")
+    @mock.patch("isse_guard_transfer.arg_parser")
+    def test_programlock_true(self, mock_arg, mock_help, mock_lock):
+
+        """Function:  test_programlock_true
+
+        Description:  Test with ProgramLock returns True.
+
+        Arguments:
+
+        """
+
+        mock_arg.arg_parse2.return_value = self.args_array
+        mock_help.return_value = False
+        mock_arg.arg_require.return_value = False
+        mock_arg.arg_valid_val.return_value = True
+        mock_arg.arg_dir_chk_crt.return_value = False
+        mock_lock.return_value = self.proglock
+
+        self.assertFalse(isse_guard_transfer.main())
+
+    @mock.patch("isse_guard_transfer.run_program",
+                mock.Mock(return_value=True))
+    @mock.patch("isse_guard_transfer.gen_class.ProgramLock")
+    @mock.patch("isse_guard_transfer.gen_libs.help_func")
+    @mock.patch("isse_guard_transfer.arg_parser")
+    def test_programlock_false(self, mock_arg, mock_help, mock_lock):
+
+        """Function:  test_programlock_false
+
+        Description:  Test with ProgramLock returns False.
+
+        Arguments:
+
+        """
+
+        mock_arg.arg_parse2.return_value = self.args_array
+        mock_help.return_value = False
+        mock_arg.arg_require.return_value = False
+        mock_arg.arg_valid_val.return_value = True
+        mock_arg.arg_dir_chk_crt.return_value = False
+        mock_lock.side_effect = \
+            isse_guard_transfer.gen_class.SingleInstanceException
+
+        with gen_libs.no_std_out():
+            self.assertFalse(isse_guard_transfer.main())
+
+    @mock.patch("isse_guard_transfer.run_program",
+                mock.Mock(return_value=True))
+    @mock.patch("isse_guard_transfer.gen_class.ProgramLock")
+    @mock.patch("isse_guard_transfer.gen_libs.help_func")
+    @mock.patch("isse_guard_transfer.arg_parser")
+    def test_programlock_id(self, mock_arg, mock_help, mock_lock):
+
+        """Function:  test_programlock_id
+
+        Description:  Test ProgramLock with flavor ID.
+
+        Arguments:
+
+        """
+
+        mock_arg.arg_parse2.return_value = self.args_array2
+        mock_help.return_value = False
+        mock_arg.arg_require.return_value = False
+        mock_arg.arg_valid_val.return_value = True
+        mock_arg.arg_dir_chk_crt.return_value = False
+        mock_lock.return_value = self.proglock
 
         self.assertFalse(isse_guard_transfer.main())
 
