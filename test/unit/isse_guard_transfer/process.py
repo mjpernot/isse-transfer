577c0ef0e67897df4c1c54c863c716e9843c9a1b
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2021-10-15 16:20:35 +0100

    Update test/unit/isse_guard_transfer/process.py v3.0.5

--- test/unit/isse_guard_transfer/process.py
+++ test/unit/isse_guard_transfer/process.py
@@ -41,11 +41,11 @@ class Logger(object):
     Description:  Class which is a representation of gen_class.Logger class.
 
     Methods:
-        __init__ -> Initialize configuration environment.
-        log_err -> log_err method.
-        log_warn -> log_warn method.
-        log_info -> log_info method.
-        log_close -> log_close method.
+        __init__
+        log_err
+        log_warn
+        log_info
+        log_close
 
     """
 
@@ -56,11 +56,6 @@ class Logger(object):
         Description:  Initialization instance of the IsseGuard class.
 
         Arguments:
-            (input) job_name -> Instance name.
-            (input) job_log -> Log name.
-            (input) log_type -> Log type.
-            (input) log_format -> Log format.
-            (input) log_time -> Time format.
 
         """
 
@@ -78,7 +73,6 @@ class Logger(object):
         Description:  log_err method.
 
         Arguments:
-            (input) data -> Log entry.
 
         """
 
@@ -91,7 +85,7 @@ class Logger(object):
         Description:  log_warn method.
 
         Arguments:
-            (input) data -> Log entry.
+            (input) data
 
         """
 
@@ -104,7 +98,6 @@ class Logger(object):
         Description:  log_info method.
 
         Arguments:
-            (input) data -> Log entry.
 
         """
 
@@ -130,14 +123,14 @@ class UnitTest(unittest.TestCase):
     Description:  Class which is a representation of a unit testing.
 
     Methods:
-        setUp -> Initialize testing environment.
-        test_bices_error -> Test with bices remove error.
-        test_bices_files -> Test with bices files.
-        test_no_files -> Test with no files processed.
+        setUp
+        test_bices_error
+        test_bices_files
+        test_no_files
         test_transfer_file_true-> Test with transfer file is set to True.
-        test_is_file -> Test with is file check.
-        test_pattern_search -> Test with pattern search.
-        test_one_file -> Test with one file check.
+        test_is_file
+        test_pattern_search
+        test_one_file
 
     """
 
@@ -158,11 +151,11 @@ class UnitTest(unittest.TestCase):
             Description:  Class which is a representation of SFTP class.
 
             Methods:
-                __init__ -> Initialize configuration environment.
-                open_conn -> open_conn method.
-                chg_dir -> chg_dir method.
-                get_pwd -> get_pwd method.
-                put_file -> put_file method.
+                __init__
+                open_conn
+                chg_dir
+                get_pwd
+                put_file
 
             """
 
@@ -173,8 +166,6 @@ class UnitTest(unittest.TestCase):
                 Description:  Initialization instance of the IsseGuard class.
 
                 Arguments:
-                    (input) cfg_file -> SFTP configuration file.
-                    (input) cfg_dir -> Path to SFTP configuration file.
 
                 """
 
@@ -204,7 +195,6 @@ class UnitTest(unittest.TestCase):
                 Description:  chg_dir method.
 
                 Arguments:
-                    (input) dir_path -> Directory path.
 
                 """
 
@@ -246,7 +236,7 @@ class UnitTest(unittest.TestCase):
             Description:  Class which is a representation of IsseGuard class.
 
             Methods:
-                __init__ -> Initialize configuration environment.
+                __init__
 
             """
 
